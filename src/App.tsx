1dc1a5968cef7116bf4b8cdf52dbfb69620a5512
Arif Aqeel <arifaqeel1233@gmail.com>
2025-07-16 13:10:14 +0500

    Initial Changes by ARIF

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='mb-10'>ARIF AQEEL AHMAD </h1>
+      <p >
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, earum.
+      </p>
+    </>
+  )
+}
+
+export default App
